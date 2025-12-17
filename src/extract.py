474d2e46a3da492bcad833ed0caa93fcd7aed46c
Barbara Sanchez <129069840+SanchezBarbara@users.noreply.github.com>
2025-12-17 12:34:43 +0100

    Delete src directory

--- src/extract.py
+++
@@ -1,8 +0,0 @@
-# Contenido de src/extract.py
-
-import pandas as pd
-
-def load_data(file_path: str) -> pd.DataFrame:
-    """Carga el DataFrame desde la ruta especificada."""
-    df = pd.read_csv(file_path, low_memory=False)
-    return df
--- src/load.py
+++
@@ -1,6 +0,0 @@
-# Contenido de src/load.py
-
-import pandas as pd
-
-def save_data(df: pd.DataFrame, output_path: str):
-       df.to_csv(output_path, index=False)
